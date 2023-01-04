86a110d26222d55ef7ec4aa2e035a857898831bf
Morning <morning@MStarss-iMac.local>
2023-01-04 12:34:15 +0800

    electron

---
+++ electron-project/src/Source.js
@@ -0,0 +1,17 @@
+const bannerProduct = [
+    {
+        thumbnail: 'images/bannerphoto01.jpg',
+        title: 'Speaker',
+        items: '6 items'
+    },
+    {
+        thumbnail: 'images/bannerphoto02.jpg',
+        title: 'Desktop & Laptop',
+        items: '6 items'
+    },
+    {
+        thumbnail: 'images/bannerphoto03.jpg',
+        title: 'DSLR camera',
+        items: '6 items'
+    }
+]
