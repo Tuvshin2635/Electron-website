4b540228e416d4b48981218e51931c8f9e39f8cf
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2023-01-05 08:33:21 +0800

    datajson nemsen

--- electron-project/src/App.js
+++ electron-project/src/App.js
@@ -1,9 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './Components/SearchBar';
+
 
 function App() {
-  return ( 
-    
+  return (
+    <div className='header'>
+      {/* tsagaan jijig header */}
+      <div className='header-first-line' >
+        <div> <p> Need help? Call us: (+98) 0234 456 789 </p></div>
+        <div className='header-first-line-end' >
+          <div> <p> 
+            <span> <i class="fa-thin fa-location-dot"></i> </span> <a href='#'> Our Store </a></p></div>
+          <div> <p> <i class="fa-thin fa-truck-fast"></i> <a href='#'> Track your order</a></p> </div>
+        </div>
+      </div>
+      {/* 2nd header tsenher nariin */}
+      <div className='header-second-line'>
+        <div className='header-second-line-inside'>
+          <img src='images/logo.jpg' alt='logo' />
+          <form>
+            <input placeholder='Search any thing'></input>
+            <button> Search </button>
+          </form>
+        </div>
+
+      {/* 2nd header tsenher nariin 2 */}
+      <SearchBar />
+
+      </div>
+
+
+
+    </div>
   );
 }
 
