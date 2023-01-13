3daa1e2131f92e3651e279890d58057319dfd5d2
Morning <morning@MStarss-iMac.local>
2023-01-13 08:38:48 +0800

    index

--- electron-project/src/index.js
+++ electron-project/src/index.js
@@ -11,8 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <jgkladhgklda/> */}
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
