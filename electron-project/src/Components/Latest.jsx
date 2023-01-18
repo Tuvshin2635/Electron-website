c66f412dd4f05c8d444f09149aa8c101d4cabf99
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2023-01-18 22:26:55 +0800

    day45 usestate zasvar not complete

--- electron-project/src/Components/Latest.jsx
+++ electron-project/src/Components/Latest.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import { latestPart } from "../Data/Banners";
 
-function LatestNews() {
+
+
+
+
+function LatestNews(props) {
+  const [cartList, setCartList] = useState('')
+
+  const cartClick = () => {
+    return(
+      setCartList('hi')
+    )
+  }
+
   const newsLatest = latestPart.map((data) => {
     return (
       <div>
@@ -11,12 +24,17 @@ function LatestNews() {
             <img src={data.thumbnail} />
           </div>
           <div className="latestText">
-            <h1> {data.date}</h1>
-            <h2> {data.title}</h2>
+            <h1> {data.date} {cartList}</h1>
+            <h2 > {data.title}</h2>
             <p> {data.description}</p>
-            <h3> {data.comment}</h3>
+            <h3> {data.comment} </h3>
+            {/* <button onClick={() => setCartList("setCartList tushilt")}> Add Cart </button>
+<h1> {cartList} </h1> */}
           </div>
         </div>
+<button onClick={cartClick}> cartclick </button>
+<h1> {cartList}</h1>
+
       </div>
     );
   });
--- electron-project/src/links/Cart.jsx
+++ electron-project/src/links/Cart.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { mainProduct } from "../Data/Banners";
+import {cartClick} from "../Components/Latest";
+
+
 
 export default function Cart() {
   
   const chosenProduct = mainProduct.map((data) => {
-    return <div>{data.title}</div>;
+    return <div>
+      {data.title}Í
+      {/* <h1> {setCartList} </h1> */}
+      {/* <p> {cartList}</p> */}
+    </div>;
+    
   });
 
   return (
