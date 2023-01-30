12f3243d93c99970ec720f08a97b34938ba3dffe
Morning <morning@MStarss-iMac.local>
2023-01-30 12:42:16 +0800

    day52

--- jobs_tab/src/App.js
+++ jobs_tab/src/App.js
@@ -5,31 +5,35 @@ import Header from "./component/header";
 import DATA from "./data/data";
 
 function App() {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
   // const [show1, setShow1] = useState(false);
   // const [show2, setShow2] = useState(false);
+  const titleMajor = DATA.map((data) => {
+    return (
+      <div>
+        {/* <p> {data.order} </p>
+        <p> {data.title} </p> */}
+        <p> {data.company} </p>
+      </div>
+    );
+  });
 
   return (
     <div className="App">
-      {show ? <ButtonShow setShow={setShow} /> : <div> hi </div>}
-      {/* {PRODUCTS.map((product) => (
-        <Product data={product} />
-      ))} */}
-      {/* {!show ? (
-        <ButtonShow setShow={setShow} />
-      ) : (
-        <div>
-          <Header />
-          exnii paragrat
-        </div>
-      )}
       {!show ? (
-        <ButtonShow setShow={setShow} />
-      ) : (
         <div>
-          <Header />
-          exnii paragrat
+          <ButtonShow setShow={setShow} />
         </div>
+      ) : (
+        <div> {titleMajor} </div>
+      )}
+
+      {/* {!show ? (
+        <div>
+          <ButtonShow setShow={setShow} />
+        </div>
+      ) : (
+        <div> {titleMajor} </div>
       )} */}
     </div>
   );
--- jobs_tab/src/component/buttonShow.jsx
+++ jobs_tab/src/component/buttonShow.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 
 import { Button } from "@mui/material";
+import DATA from "../data/data";
 
-export default function ButtonShow({ setShow }) {
+export default function ButtonShow({ show, setShow }) {
+  const titleMajor = DATA.map((data) => {
+    return (
+      <div>
+        <p> {data.company} </p>
+      </div>
+    );
+  });
+
   return (
     <div>
       <Button
         onClick={() => {
-          setShow(false);
+          setShow(true);
+          if (!show) {
+            const showData = {
+              titleMajor,
+            };
+          }
         }}
       >
         showooo
       </Button>
+      {/* <Button
+        onClick={() => {
+          setShow(true);
+        }}
+      >
+        showooo2
+      </Button> */}
     </div>
   );
 }
